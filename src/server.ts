61171d9916ae59a84c6131af848d1d0ee4186d12
lucascprazeres <olucascostadp@gmail.com>
2020-09-03 00:10:49 -0300

    backend: configures eslint, prettier and .editorconfig

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,9 @@ const app = express();
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  return res.json({ ok: true });
-})
+app.get('/', (req, res) => res.json({ ok: true }));
 
 app.listen(3333, () => {
-  console.log(`API is running on port ${3333}`)
-})
+  // eslint-disable-next-line no-console
+  console.log(`API is running on port ${3333}`);
+});
